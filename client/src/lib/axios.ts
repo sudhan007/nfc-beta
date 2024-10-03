381f07ea02b3a9ea45a95959d0438703e1818cb7
rasla <programmer.rasla@gmail.com>
2024-10-03 15:00:53 +0530

    added

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // export const BASE_URL = "http://localhost:4000/api";
-//export const BASE_URL = "https://app.wenoxo.in/api";
- export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://app.wenoxo.in/api";
+//  export const BASE_URL = "http://localhost:4000/api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
