b2929a4ea8431cf8a9ba248bc1190209895711f5
rasla <programmer.rasla@gmail.com>
2024-10-14 11:01:15 +0530

    added final pressup changs

--- client/src/routes/pressup.lazy.tsx
+++ client/src/routes/pressup.lazy.tsx
@@ -6,41 +6,69 @@ export const Route = createLazyFileRoute("/pressup")({
 
 function Pressup() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-50 p-4 mt-[20vh]">
-      <div className="w-full flex justify-center mb-8">
-        <img
-          src="/pressup.jpg"
-          alt="Logo"
-          className="w-32 h-32 rounded-lg shadow-lg shadow-black/30"
-        />
-      </div>
-
-      <div className="flex gap-4">
-        <a
-          href="https://m.9m.io/kug2pu7"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block"
-        >
+    <div className="bg-black font-body">
+      <div className="min-h-screen flex flex-col bg-gray-100 w-screen md:w-[430px] mx-auto">
+        <div className="flex items-center py-4 px-6 gap-4">
           <img
-            src="/google-play.svg"
-            alt="Google Play"
-            className="w-40 h-auto cursor-pointer"
+            src="/pressup.jpg"
+            alt="Logo"
+            className="w-16 h-16 rounded-lg shadow-md shadow-black/30"
           />
-        </a>
 
-        <a
-          href="https://m.9m.io/ku7pkso"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block"
-        >
-          <img
-            src="/app-store.svg"
-            alt="App Store"
-            className="w-40 h-auto cursor-pointer"
-          />
-        </a>
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold font-body">Pressup</h1>
+            <p
+              className="text-sm font-body font-regular"
+              style={{
+                fontFamily: "Ex",
+              }}
+            >
+              ONE STOP IRONING SOLUTIONS
+            </p>
+          </div>
+        </div>
+
+        {/* Main Content Section */}
+        <div className="flex-grow flex flex-col items-center justify-center  p-4">
+          <div className="w-full flex justify-center">
+            <img
+              src="/pressup-home.png"
+              alt="Home Screen"
+              className="w-64 h-auto rounded-lg shadow-lg shadow-black/30"
+            />
+          </div>
+        </div>
+
+        {/* Footer Section with Download Links */}
+        <div className="w-full p-4">
+          <div className="flex gap-6 justify-center">
+            <a
+              href="https://m.9m.io/kug2pu7"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block"
+            >
+              <img
+                src="/google-play.svg"
+                alt="Google Play"
+                className="w-40 h-auto cursor-pointer transition-transform transform hover:scale-105"
+              />
+            </a>
+
+            <a
+              href="https://m.9m.io/ku7pkso"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block"
+            >
+              <img
+                src="/app-store.svg"
+                alt="App Store"
+                className="w-40 h-auto cursor-pointer transition-transform transform hover:scale-105"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
